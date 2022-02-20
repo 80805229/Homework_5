ca11b05b08755935ee030dcfdd0cab6e68baa0a0
Anna <aanikonova15@gmail.com>
2022-02-20 20:40:11 +0300

    Add Task_40

---
+++ Task_40/Program.cs
@@ -0,0 +1,41 @@
+﻿// В указанном массиве вещественных чисел найти разницу между максимальным и минимальным элементами.
+
+void PrintArray(double[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write(mass[i] + "\t");
+    }
+}
+
+double[] array = { 8.6, 5.9, 8.4, 2.6, 7.6, 4.1, 6.8 };
+PrintArray(array);
+
+Console.WriteLine();
+
+int ind = 1;
+double max = array[0];
+double min = array[0];
+double dif = 0;
+
+while (ind < array.Length)
+{
+    if (array[ind] > max)
+    {
+        max = array[ind];
+    }
+
+    if (array[ind] < min)
+    {
+        min = array[ind];
+    }
+    ind++;
+    dif = max - min;
+}
+
+
+
+Console.WriteLine("Максимальное число равно " + max);
+Console.WriteLine("Минимальное число равно " + min);
+Console.WriteLine("Разница между max и min равна " + dif);
+
